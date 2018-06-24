d149858b9effb86956b5f029cd7b246abdc3c080
saeedseyfi <me@saeedseyfi.com>
2018-06-24 22:36:19 +0430

    Use parent package

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const exec = require('child_process').execFile;
+const parentPackagePath = require('parent-package-json').path;
 
-module.exports = async function (bin, oprions = {timeout: 5000}) {
+module.exports = async function (bin, options = {timeout: 5000}) {
     return new Promise((resolve, reject) => {
-        exec(`./node_modules/.bin/${bin}`, oprions, (error, stdout) => {
+        exec(`${parentPackagePath}/node_modules/.bin/${bin}`, options, (error, stdout) => {
             if (error) {
                 reject(error);
                 return;
