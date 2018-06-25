0f543f432ed241d819f45f7aec868b0b7f4cb492
saeedseyfi <me@saeedseyfi.com>
2018-06-25 22:03:52 +0430

    Print live stderror

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const exec = require('child_process').exec;
 
 module.exports = async function (bin, options) {
     return new Promise((resolve, reject) => {
-            const cmd = `${process.cwd()}/node_modules/.bin/${bin}`;
+        const cmd = `${process.cwd()}/node_modules/.bin/${bin}`;
 
-            console.log(`Running \`${cmd}\``);
+        console.log(`Running \`${cmd}\``);
 
-            exec(cmd, options, (error, stdout) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
+        const theProcess = exec(cmd, options, (error, stdout, stderr) => {
+            if (stderr) {
+                reject(error);
+                return;
+            }
 
-                resolve(stdout);
-            }).stdout.pipe(process.stdout);
-        }
-    );
+            resolve(stdout);
+        });
+
+        theProcess.stdout.on('data', (data) => {
+            process.stdout.write(data.toString());
+        });
+
+        theProcess.stderr.on('data', (data) => {
+            process.stdout.write(data.toString());
+        });
+    });
 };
