d425866375c85038133a6f79db2aac66c0a72624
Saeed Seyfi <saeeds@spotify.com>
2023-01-04 17:43:01 +0100

    fixes(CVE-2022-25923): resolves command injection security issue

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const exec = require('child_process').exec;
+const fs = require('fs');
+const path = require('path');
 
 module.exports = async function (bin, options) {
-    return new Promise((resolve, reject) => {
-        const cmd = `${process.cwd()}/node_modules/.bin/${bin}`;
+    return new Promise(async (resolve, reject) => {
+        const binDir = `${process.cwd()}/node_modules/.bin`;
+        const cmd = path.join(binDir, bin);
+
+        if (!cmd.startsWith(binDir)) {
+            reject(new Error(`${cmd} within the expected directory`));
+            return;
+        }
+
+        try {
+            await fs.access(cmd, fs.constants.X_OK);
+        } catch (err) {
+            reject(new Error(`${cmd} is not accessible: ${err.message}`));
+            return;
+        }
 
         console.log(`Running \`${cmd}\``);
 
